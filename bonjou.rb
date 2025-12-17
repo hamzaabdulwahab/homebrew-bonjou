96f759e7ae91351a74d519bacc1e22fa3b44e9fd
Hamza Wahab <hamzaabdulwahab19@gmail.com>
2025-12-17 09:22:12 +0500

    update to v1.0.0 with new repo URL

--- bonjou.rb
+++ bonjou.rb
@@ -1,11 +1,11 @@
 class Bonjou < Formula
   desc "Terminal-based LAN chat and transfer application"
-  homepage "https://github.com/hamzawahab/bonjou-terminal"
-  version "1.0.10"
+  homepage "https://github.com/hamzaabdulwahab/bonjou-cli"
+  version "1.0.0"
 
   on_macos do
-  url "https://github.com/hamzaabdulwahab/bonjou-terminal/releases/download/v1.0.10/bonjou-macos.tar.gz"
-  sha256 "9f73609038a7134324aec8c56c99a9fc538f649622f5cabce972be6a4df3bd57"
+    url "https://github.com/hamzaabdulwahab/bonjou-cli/releases/download/v1.0.0/bonjou-macos"
+    sha256 "0c6e2d77c2cd1b1b03253eb7685b365641815963c3381f2943220632bebfcf7d"
   end
 
   def install
