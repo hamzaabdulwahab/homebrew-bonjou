a74e59742ebe68becf9d42e165e975386d9b0b57
Hamza Wahab <hamzaabdulwahab19@gmail.com>
2025-12-20 13:49:42 +0500

    Fix formula location and checksum

--- Formula/bonjou.rb
+++ Formula/bonjou.rb
@@ -1,16 +1,16 @@
 class Bonjou < Formula
-  desc "Terminal-based LAN chat and transfer application"
+  desc "Terminal-based LAN chat application with peer discovery"
   homepage "https://github.com/hamzaabdulwahab/bonjou-cli"
   version "1.0.0"
   license "MIT"
 
   on_macos do
     url "https://github.com/hamzaabdulwahab/bonjou-cli/releases/download/v1.0.0/bonjou-macos"
-    sha256 "e2830fefb316b103e6a34aca7ae46c76ca9d36a73b7149e1642d2baf223fb9fb"
+    sha256 "6b039830f65fb1224027fb4a9aa4233e84fe486956d380a408ae7ef087dbd43c"
+  end
 
-    def install
-      bin.install "bonjou-macos" => "bonjou"
-    end
+  def install
+    bin.install "bonjou-macos" => "bonjou"
   end
 
   test do
